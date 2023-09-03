48577b247b3f2194b521db2fbf5ff020b26b5925
Jaber <f.mj620@gmail.com>
2023-09-03 12:38:01 +0300

    Initial Commit

---
+++ Montak/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Montak
+//
+//  Created by Blue Ray on 03/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
